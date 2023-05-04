f3490f1e88bb6898221ba213eb70286c887d760b
Powwow84 <pao.saelee84@gmail.com>
2023-05-04 13:08:48 -0700

    got the redirect working and the object in the new page

--- src/components/pages/SearchResults.jsx
+++ src/components/pages/SearchResults.jsx
@@ -1,9 +1,21 @@
-export default function SearchResults(props){
+import { useLocation } from "react-router-dom";
 
+export default function SearchResults() {
+  const location = useLocation();
+  const foundParks = location.state?.foundParks;
 
-    return(
-        <>
-        <p>Your at the right place to see results</p>
-        </>
-    )
+  return (
+    <>
+      <h2>Here are your search results</h2>
+      {foundParks &&
+        foundParks.map((park) => (
+          <div key={park.id}>
+            <h3>{park.fullName}</h3>
+            <p>
+              {park.addresses[0].city}, {park.addresses[0].stateCode}
+            </p>
+          </div>
+        ))}
+    </>
+  );
 }

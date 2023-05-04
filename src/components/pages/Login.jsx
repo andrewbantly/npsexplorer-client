1774ad3873167f7bcc96b81c16a9de93df400137
abdallah1936 <awadallaabdallah@gmail.com>
2023-05-04 11:34:22 -0700

    Styling

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -2,76 +2,145 @@ import { useState } from 'react'
 import axios from 'axios'
 import jwt_decode from 'jwt-decode'
 import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components';
+
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f7f7f7;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  font-weight: 700;
+  color: #484848;
+  margin-bottom: 16px;
+`;
+
+const ErrorMessage = styled.p`
+  color: #ff5a5f;
+  margin-bottom: 16px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 400px;
+`;
+
+const Label = styled.label`
+  font-size: 16px;
+  font-weight: 600;
+  color: #484848;
+  margin-bottom: 8px;
+`;
+
+const Input = styled.input`
+  font-size: 16px;
+  padding: 12px 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 16px;
+  outline: none;
+  &:focus {
+    border-color: #008489;
+  }
+`;
+
+const SubmitButton = styled.button`
+  font-size: 16px;
+  font-weight: 600;
+  background-color: #ff5a5f;
+  color: #fff;
+  padding: 12px 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+  &:hover {
+    background-color: #ff3d48;
+  }
+`;
+
+const Image = styled.img`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999; 
+  width: 100px; 
+  height: 100px;
+`;
 
 export default function Login({ currentUser, setCurrentUser }) {
-	// state for the controlled form
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [msg, setMsg] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [msg, setMsg] = useState('');
+  
+    const navigate = useNavigate();
+  
+    const handleSubmit = async e => {
+      e.preventDefault();
+      try {
+        const reqBody = {
+          email,
+          password,
+        };
+        const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/login`, reqBody);
+  
+        const { token } = response.data;
+        localStorage.setItem('jwt', token);
+  
+        const decoded = jwt_decode(token);
+  
+        setCurrentUser(decoded);
+      } catch (err) {
+        console.warn(err);
+        if (err.response) {
+          setMsg(err.response.data.msg);
+        }
+      }
+    };
+  
+    if (currentUser) {
+      navigate('/users/profile');
+    }
+  
 
-	const navigate = useNavigate()
+    return (
+        <Container>
+            <Image src="https://www.nps.gov/common/uploads/structured_data/68BFC1AC-BF96-629F-89D261D78F181C64.jpg" alt="Default Image" />
 
-	// submit event handler
-	const handleSubmit = async e => {
-		e.preventDefault()
-		try {
-			// post fortm data to the backend
-			const reqBody = {
-				email, 
-				password
-			}
-			const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/login`, reqBody)
+            <Title>Login to Your Account:</Title>
 
-			// save the token in localstorage
-			const { token } = response.data
-			localStorage.setItem('jwt', token)
+            <ErrorMessage>{msg}</ErrorMessage>
 
-			// decode the token
-			const decoded = jwt_decode(token)
+            <Form onSubmit={handleSubmit}>
+                <Label htmlFor='email'>Email:</Label>
+                <Input
+                    type='email'
+                    id='email'
+                    placeholder='your email...'
+                    onChange={e => setEmail(e.target.value)}
+                    value={email}
+                />
+                <Label htmlFor='password'>Password:</Label>
+                <Input
+                    type='password'
+                    id='password'
+                    placeholder='password...'
+                    onChange={e => setPassword(e.target.value)}
+                    value={password}
+                />
 
-			// set the user in App's state to be the decoded token
-			setCurrentUser(decoded)
-
-		} catch (err) {
-			console.warn(err)
-			if (err.response) {
-				setMsg(err.response.data.msg)
-			}
-		}
- 	}
-
-	// conditionally render a navigate component
-	if (currentUser) {
-		navigate("/users/profile")
-	}
-
-	return (
-		<div>
-			<h1>Login to Your Account:</h1>
-
-			<p>{msg}</p>
-
-			<form onSubmit={handleSubmit}>
-				<label htmlFor='email'>Email:</label>
-				<input 
-					type="email"
-					id="email"
-					placeholder='your email...'
-					onChange={e => setEmail(e.target.value)}
-					value={email}
-				/>
-
-				<label htmlFor='password'>Password:</label>
-				<input 
-					type="password"
-					id="password"
-					placeholder='password...'
-					onChange={e => setPassword(e.target.value)}
-					value={password}
-				/>
-
-				<button type="submit">Login</button>
-			</form>
-		</div>
-	)
+                <SubmitButton type='submit'>Login</SubmitButton>
+            </Form>
+        </Container>
+    )
 }

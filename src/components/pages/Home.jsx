c77f90794796033b78cf65b1de54c153ae53735d
Powwow84 <pao.saelee84@gmail.com>
2023-05-04 21:54:29 -0700

    added carousel package

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export default function Home(props) {
   const { parksInfo } = props;
@@ -104,12 +106,45 @@ const handleActivity = (activityName) => {
         ));
       };
       
+
+      const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 23,
+          slidesToSlide: 3 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+      };
+     
    
 
 
     return(
         <div className='container'>
         <div className='searchBar'>
+        <Carousel responsive={responsive} centerMode={true} arrows={true} containerClass='carousel'>
+        <div className="activityIcon">
+            Hiking
+            </div>
+            <div className="activityIcon">
+                <p>Swim</p>
+            </div>
+            <div className="activityIcon">
+                <p>Ski</p>
+            </div>
+            <div className="activityIcon">
+                <p>Camp</p>
+            </div>
+      </Carousel>
         <form>
             <input
                 id='name'

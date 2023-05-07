55a69b2748380f7407f8c1596b5bfde8b5a05583
abdallah1936 <awadallaabdallah@gmail.com>
2023-05-07 11:31:51 -0700

    Destinations message

--- src/components/pages/Destinations.jsx
+++ src/components/pages/Destinations.jsx
@@ -10,7 +10,15 @@ const DestinationsPage = (props) => {
          handleAddExperienceClick,
          setUserDestinations} = props
 
-    const removeDestination = async (destinationId) => {
+//const removeDestination = async (destinationId, park) => { ... }: This line defines an asynchronous function called removeDestination. This function takes two arguments: destinationId (the ID of the destination to be removed) and park (an object containing the details of the park associated with the destination).
+
+//Retrieve the JSON Web Token (JWT) from the browser's local storage.
+
+//After successfully deleting the destination, the code sends another request to the server to fetch the updated list of user destinations using the axios.get method
+
+//The updated list of destinations is then passed to the setUserDestinations function to update the state in the parent component.
+
+    const removeDestination = async (destinationId, park) => {
         try {
             const token = localStorage.getItem('jwt');
             const options = {
@@ -23,22 +31,37 @@ const DestinationsPage = (props) => {
             const foundDestinations = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/destinations`, options)
             setUserDestinations(foundDestinations.data)
 
-            setMessage('Destination removed from favorites');
+            // console.log(foundDestinations)
+
+            setMessage(`${park.fullName} was removed from favorites`);
         } catch (error) {
             setMessage('Error removing destination from favorites');
         }
     };
+    
+    // the findParkById function takes destinationId as an argument, destionsIs is the Id of the park we want to find
+    // the findIndex method is used on the parksInfo array. This method goes through each element "park" in the array and checks if the id of the park is equal to the destinationId. If it finds a match, it returns the index of that element "park" in the array. The result is stored in the parkIndex variable.
+    //  The originalIndex property holds the value of parkIndex, which is the original index of the park in the parksInfo array.
+    // returns this object containing the park and originalIndex properties.
 
     const findParkById = (destinationId) => {
-        return parksInfo.find((park) => park.id === destinationId);
+        const parkIndex = parksInfo.findIndex((park) => park.id === destinationId);
+        return {
+            park: parksInfo[parkIndex],
+            originalIndex: parkIndex
+        };
     };
+    
+    //The userDestinations array is being mapped over, which means a new array is being created by transforming each destination
+    // For each destination, the findParkById function is called it passes the destination as an argument. This returns an object containing the park and originalIndex properties
+    // The returned object is then destructured using the syntax const { park, originalIndex } = findParkById(destination);. This means that the park and originalIndex properties are extracted from the returned object 
 
-    const userDestinationsList = userDestinations.map((destination, index) => {
-        const park = findParkById(destination);
+    const userDestinationsList = userDestinations.map((destination) => {
+        const { park, originalIndex } = findParkById(destination);
         return (
             <div className='parkContainer'>
                 <button
-                    onClick={() => removeDestination(destination)}
+                    onClick={() => removeDestination(destination, park)}
                     className="removeButton"
                 >
                     Remove from Destinations
@@ -49,7 +72,8 @@ const DestinationsPage = (props) => {
                 >
                     Add to Experience
                 </button>
-                <Link to={`/parks/${park?.fullName}/${index}`} key={`${park?.id}-${index}`}>
+                
+                <Link to={`/parks/${park?.fullName}/${originalIndex}`} key={`${park?.id}-${originalIndex}`}>
 
                     <div>
                         <img
@@ -80,6 +104,7 @@ const DestinationsPage = (props) => {
 
             <h2>User Destinations</h2>
             <ul>
+                <p>{message}</p>
                 {userDestinationsList}
             </ul>
 

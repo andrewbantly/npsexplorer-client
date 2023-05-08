ebc7de6b4da9ff3553f46a1aef32100db0f941d8
abdallah1936 <awadallaabdallah@gmail.com>
2023-05-08 09:47:23 -0700

    style

--- src/components/pages/Destinations.jsx
+++ src/components/pages/Destinations.jsx
@@ -17,7 +17,7 @@ const ParkContainer = styled.div`
     border-radius: 10px;
     position: relative;
     background-color: #f0f0f0;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
 
     @media (max-width: 480px) {
         width: 90vw;
@@ -135,8 +135,11 @@ const DestinationsPage = (props) => {
     };
 
     // the findParkById function takes destinationId as an argument, destionsIs is the Id of the park we want to find
+
     // the findIndex method is used on the parksInfo array. This method goes through each element "park" in the array and checks if the id of the park is equal to the destinationId. If it finds a match, it returns the index of that element "park" in the array. The result is stored in the parkIndex variable.
+
     //  The originalIndex property holds the value of parkIndex, which is the original index of the park in the parksInfo array.
+
     // returns this object containing the park and originalIndex properties.
 
     const findParkById = (destinationId) => {
@@ -148,7 +151,9 @@ const DestinationsPage = (props) => {
     };
 
     //The userDestinations array is being mapped over, which means a new array is being created by transforming each destination
+
     // For each destination, the findParkById function is called it passes the destination as an argument. This returns an object containing the park and originalIndex properties
+    
     // The returned object is then destructured using the syntax const { park, originalIndex } = findParkById(destination);. This means that the park and originalIndex properties are extracted from the returned object 
 
     const userDestinationsList = userDestinations.map((destination) => {
@@ -178,7 +183,7 @@ const DestinationsPage = (props) => {
                             Add
                         </ExperienceButton>
                     </ButtonsContainer>
-                    
+
                     <ParkText>
                         <h3>{park?.fullName}</h3>
                         <div>
